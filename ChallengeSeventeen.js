479bad734b8ee2b7ccb98c4bd68dee857e25fb6f
SarahMcQ <39204732+SarahMcQ@users.noreply.github.com>
2019-11-30 19:19:12 -0500

    Add files via upload

---
+++ ChallengeSeventeen.js
@@ -0,0 +1,12 @@
+// #17
+// Instructions
+// For this challenge, we'll need to implement a function called judgeVegetable()that will decide which vegetable is best based on a given judging characteristic. Our function will receive two parameters: a list of veggies(as an array of objects), and a characteristic to judge the veggies by (in the case of a tomato, either redness or plumpness).
+
+// Our function must return the name of the person who submitted (vegetables.submitter) the vegetable with the highest ranking in the provided category.  
+const judgeVegetable = (vegetables, metric) => {
+
+
+    return (vegetables.reduce((Leader, Submission) =>
+        Submission[metric] > Leader[metric] ?
+        Submission : Leader)).submitter
+};
